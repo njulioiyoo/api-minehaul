2ba117fb81f19d1d2395856d45cf21b119c4a30c
Julio Notodiprodyo <julio.notodiprodyo@koltiva.com>
2024-08-12 00:47:45 +0700

    feat(seeder): add 100 fake records for devices table

--- database/seeders/DevicesTableSeeder.php
+++ database/seeders/DevicesTableSeeder.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -14,23 +15,28 @@ class DevicesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('devices')->insert([
-            'device_id' => 1,
-            'account_id' => null,
-            'device_type_id' => 0,
-            'device_display_id' => 'DEV-001',
-            'device_name' => 'Device Testing',
-            'device_sim_id' => '001',
-            'device_year' => 2024,
-            'device_make_id' => 1,
-            'device_model_id' => 1,
-            'device_status_id' => 2,
-            'dt_status' => 'active',
-            'dt_creator' => 1,
-            'dt_create_date' => '2024-07-15 20:33:15',
-            'dt_editor' => 1,
-            'dt_edit_date' => '2024-07-16 20:31:14',
-            'uid' => 'b7db5ddaf4d705d6950074f0e4ccb19c',
-        ]);
+        $faker = Faker::create();
+
+        $statuses = ['active', 'inactive', 'nullified'];
+
+        for ($i = 0; $i < 100; $i++) {
+            DB::table('devices')->insert([
+                'account_id' => $faker->optional()->randomNumber(),
+                'device_type_id' => $faker->optional()->randomNumber(),
+                'device_display_id' => $faker->optional()->bothify('??-#####'),
+                'device_name' => $faker->optional()->word,
+                'device_sim_id' => $faker->optional()->bothify('SIM-########'),
+                'device_year' => $faker->optional()->year,
+                'device_make_id' => $faker->optional()->randomNumber(),
+                'device_model_id' => $faker->optional()->randomNumber(),
+                'device_status_id' => $faker->optional()->randomNumber(),
+                'dt_status' => $faker->randomElement($statuses),
+                'dt_creator' => $faker->optional()->randomNumber(),
+                'dt_create_date' => $faker->optional()->dateTime,
+                'dt_editor' => $faker->optional()->randomNumber(),
+                'dt_edit_date' => $faker->optional()->dateTime,
+                'uid' => $faker->optional()->uuid,
+            ]);
+        }
     }
 }
