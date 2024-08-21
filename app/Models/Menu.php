f66c00336b07227908be0abb8a24019348fa1269
Julio Notodiprodyo <julio.notodiprodyo@koltiva.com>
2024-08-21 18:45:16 +0700

    feat(menu): extract role and permission sync logic to trait

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -4,81 +4,23 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Models\Traits\HasMenuAttributes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Str;
 use Spatie\Permission\Models\Role;
 
 class Menu extends Model
 {
     use HasFactory;
+    use HasMenuAttributes;
     use SoftDeletes;
 
     protected $fillable = ['name', 'key', 'url', 'parent_id', 'position', 'deleted_at'];
 
     public $timestamps = false;
 
-    // Properti untuk menyimpan roles sementara
     protected $rolesToSync = [];
-
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::saving(function ($menu) {
-            // Generate the key from the name field
-            $menu->key = Str::slug($menu->name);
-
-            if (request()->has('data.attributes.roles')) {
-                $roles = request()->input('data.attributes.roles');
-                $menu->rolesToSync = $roles;
-            }
-        });
-
-        static::saved(function ($menu) {
-            if (! empty($menu->rolesToSync) && is_array($menu->rolesToSync)) {
-                Log::info('Syncing roles: ', $menu->rolesToSync);
-                $menu->roles()->sync($menu->rolesToSync);
-
-                // Ambil permissions yang terkait dengan roles
-                $permissions = Role::whereIn('id', $menu->rolesToSync)->with('permissions')->get()
-                    ->pluck('permissions')
-                    ->flatten()
-                    ->unique('id')
-                    ->mapWithKeys(function ($permission) {
-                        return [$permission->id => 'read'];
-                    })
-                    ->toArray();
-
-                Log::info('Permissions to sync:', $permissions);
-
-                // Sinkronisasi permissions di tabel pivot PermissionMenu
-                foreach ($permissions as $permissionId => $status) {
-                    Log::info('Updating or creating PermissionMenu with:', [
-                        'menu_id' => $menu->id,
-                        'permission_id' => $permissionId,
-                        'status' => $status,
-                    ]);
-
-                    try {
-                        PermissionMenu::updateOrCreate(
-                            ['menu_id' => $menu->id, 'permission_id' => $permissionId],
-                            ['status' => $status]
-                        );
-                    } catch (\Exception $e) {
-                        Log::error('Error syncing PermissionMenu:', [
-                            'menu_id' => $menu->id,
-                            'permission_id' => $permissionId,
-                            'status' => $status,
-                            'error' => $e->getMessage(),
-                        ]);
-                    }
-                }
-            }
-        });
-    }
 
     // Relasi menu ke role_menus
     public function roleMenu()
