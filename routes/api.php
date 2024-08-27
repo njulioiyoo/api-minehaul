3bacb1ddbebbca525cca82b69b1fa620df3b81c7
Julio Notodiprodyo <julio.notodiprodyo@koltiva.com>
2024-08-27 08:45:21 +0700

    fixed

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::middleware('json.api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:api');
 });
 
-Route::group(function () {
+Route::middleware('auth:api')->group(function () {
     Route::get('/get-users', function () {
         $users = User::all();
         return response()->json($users);
