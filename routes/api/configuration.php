e150bb49b6296168fe8da047517b07f0e59c230c
Achmad Fatkharrofiqi <ahmad.fatkharrofiqi@koltiva.com>
2024-09-02 22:59:13 +0700

    fix: change permission to slug

--- routes/api/configuration.php
+++ routes/api/configuration.php
@@ -20,9 +20,9 @@ use LaravelJsonApi\Laravel\Facades\JsonApiRoute;
 JsonApiRoute::server('v1')->middleware('validate.api', 'json.api', 'verify.user.role')->resources(function () {
     // Routes for devices
     Route::prefix('devices')->group(function () {
-        Route::get('/', [DeviceController::class, 'readDevice'])->name('device.index')->middleware('verify.user.permission:View Device');
-        Route::post('/', [DeviceController::class, 'createDevice'])->name('device.create')->middleware('verify.user.permission:Create Device');
-        Route::patch('/', [DeviceController::class, 'updateDevice'])->name('device.update')->middleware('verify.user.permission:Edit Device');
-        Route::delete('/', [DeviceController::class, 'deleteDevice'])->name('device.delete')->middleware('verify.user.permission:Delete Device');
+        Route::get('/', [DeviceController::class, 'readDevice'])->name('device.index')->middleware('verify.user.permission:view-device');
+        Route::post('/', [DeviceController::class, 'createDevice'])->name('device.create')->middleware('verify.user.permission:create-device');
+        Route::patch('/', [DeviceController::class, 'updateDevice'])->name('device.update')->middleware('verify.user.permission:edit-device');
+        Route::delete('/', [DeviceController::class, 'deleteDevice'])->name('device.delete')->middleware('verify.user.permission:delete-device');
     });
 });
