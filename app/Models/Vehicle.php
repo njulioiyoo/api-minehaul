2ea53c485f5d9a2f59617cb586f75b877b544336
Julio Notodiprodyo <julio.notodiprodyo@koltiva.com>
2024-09-03 13:35:29 +0700

    feat: create api read vehicle

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -2,6 +2,10 @@
 
 namespace App\Models;
 
+use App\Models\Vehicle\VehicleMake;
+use App\Models\Vehicle\VehicleModel;
+use App\Models\Vehicle\VehicleStatus;
+use App\Models\Vehicle\VehicleType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -17,4 +21,34 @@ class Vehicle extends Model
 
     protected $table = 'vehicles';
     protected $guarded = [];
+
+    public function account()
+    {
+        return $this->belongsTo(Account::class)->select('id', 'company_code', 'company_name');
+    }
+
+    public function pit()
+    {
+        return $this->belongsTo(Pit::class, 'pit_id')->select('id', 'name', 'description');
+    }
+
+    public function vehicleType()
+    {
+        return $this->belongsTo(VehicleType::class, 'vehicle_type_id', 'id')->select('id', 'name');
+    }
+
+    public function vehicleMake()
+    {
+        return $this->belongsTo(VehicleMake::class, 'vehicle_make_id', 'id');
+    }
+
+    public function vehicleModel()
+    {
+        return $this->belongsTo(VehicleModel::class, 'vehicle_model_id', 'id');
+    }
+
+    public function vehicleStatus()
+    {
+        return $this->belongsTo(VehicleStatus::class, 'vehicle_status_id', 'id');
+    }
 }
